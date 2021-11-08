aae92280afabe5973ea324b48edc3375f65d4286
Alexandr-oss <Aleksandarbabaleu@yandex.ru>
2021-11-08 12:50:30 +0300

    create: news

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".main-reviews__pagination",
-  },
+    // If we need pagination
+    pagination: {
+      el: ".main-reviews__pagination",
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".main-text__button_slider--next",
-    prevEl: ".main-text__button_slider--prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".main-text__button_slider--next",
+      prevEl: ".main-text__button_slider--prev",
+    },
+  });
+  var modalMenu = $("[data-toggle=menu]");
+  var closeModalMenu = $("[data-toggle=menu]");
+  modalMenu.on("click", openMenu);
+
+  function openMenu() {
+    var menuOverlay = $(".main-salon_list");
+    menuOverlay.addClass(".main-salon_list--visible");
+  }
 });
